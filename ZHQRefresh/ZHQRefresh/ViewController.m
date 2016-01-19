6314f9b60c530d60c91171a45e09c4fb5cf26186
123 <123@163.com>
2016-01-19 09:12:24 +0800

    第一版，以表视图控件为使用例子

--- ZHQRefresh/ZHQRefresh/ViewController.m
+++ ZHQRefresh/ZHQRefresh/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import "ZHQRefresh.h"//必须导入这个头文件
+@interface ViewController ()<UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,12 +17,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.tableView.dataSource=self;//设置代理
+    self.tableView.tableFooterView=[[UIView alloc]initWithFrame:CGRectZero];//关闭不需要的分割线
+    //增加下拉刷新功能
+    [self.tableView refreshWithTarget:self andWithAction:@selector(refresh)];
+    //增加上拉加载功能
+    [self.tableView loadWithTarget:self andWithAction:@selector(load)];
+    
+    //注意：refreshWithTarget loadWithTarget
+    //endRefreshing endLoading必须在主线程调用，因为里面有ui操作
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+#pragma mark - tableView
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 50;//也可以用10或0
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * cellId=@"cell";
+    UITableViewCell * cell=[tableView dequeueReusableCellWithIdentifier:cellId];
+    if(cell==nil)
+    {
+        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    cell.textLabel.text=[NSString stringWithFormat:@"%03ld",indexPath.row+1];
+    return cell;
+}
+#pragma mark - 刷新和加载具体行为
+-(void)refresh
+{
+    //在这进行你的刷新
+    [NSThread sleepForTimeInterval:2];//模拟刷新用时2秒
+    //结束刷新
+    [self.tableView endRefreshing];//必须调用这个行为结束刷新
+}
+-(void)load
+{
+    //在这进行你的加载
+    [NSThread sleepForTimeInterval:2];//模拟刷新用时2秒
+    //结束刷新
+    [self.tableView endLoading];//必须调用这个行为结束刷新
+}
 @end
